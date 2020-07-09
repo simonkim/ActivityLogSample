f3f88061de89559db85fd356ce99aca0785c81b7
Simon Kim <simon.kim@kinemaster.com>
2020-07-10 08:18:28 +0900

    Initial Commit

---
+++ ActivityLogSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityLogSample
+//
+//  Created by Simon Kim on 7/10/20.
+//  Copyright © 2020 KineMaster Crop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
